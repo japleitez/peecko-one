f852ebf11c888b78404db6940ceaa9a814faf846
jose.pleitez <jose.pleitez@gmail.com>
2026-05-02 18:38:34 +0200

    add invoice search form

---
+++ src/main/java/com/peecko/one/service/request/InvoiceListRequest.java
@@ -0,0 +1,32 @@
+package com.peecko.one.service.request;
+
+public class InvoiceListRequest {
+
+    private Long customerId;
+    private Integer starts;
+    private Integer ends;
+    private String number;
+
+    public InvoiceListRequest(Long customerId, Integer starts, Integer ends, String number) {
+        this.customerId = customerId;
+        this.starts = starts;
+        this.ends = ends;
+        this.number = number;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public Integer getStarts() {
+        return starts;
+    }
+
+    public Integer getEnds() {
+        return ends;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
---
+++ src/main/java/com/peecko/one/service/specs/InvoiceSpecs.java
@@ -0,0 +1,28 @@
+package com.peecko.one.service.specs;
+
+import com.peecko.one.domain.Invoice;
+import com.peecko.one.domain.Invoice_;
+import org.springframework.data.jpa.domain.Specification;
+
+public class InvoiceSpecs extends BaseSpecs {
+
+    public static Specification<Invoice> agencyId(Long id) {
+        return (root, query, cb) -> cb.equal(root.get(Invoice_.AGENCY_ID), id);
+    }
+
+    public static Specification<Invoice> customerId(Long id) {
+        return (root, query, cb) -> cb.equal(root.get(Invoice_.CUSTOMER_ID), id);
+    }
+
+    public static Specification<Invoice> starts(Integer period) {
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(root.get(Invoice_.PERIOD), period);
+    }
+
+    public static Specification<Invoice> ends(Integer period) {
+        return (root, query, cb) -> cb.lessThanOrEqualTo(root.get(Invoice_.PERIOD), period);
+    }
+
+    public static Specification<Invoice> numberLike(String number) {
+        return (root, query, cb) -> cb.like(root.get(Invoice_.NUMBER), anyText(number));
+    }
+}
